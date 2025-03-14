28707bd301df2a923c3243bf5cc343f06161a530
mikeheul <mickael.murmann@gmail.com>
2025-03-14 11:02:56 +0100

    Add movie poster and plot details to movie search results

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,27 @@ document.addEventListener('alpine:init', () => {
         async fetchMovies() {
             if (this.searchQuery.length >= 3) {
                 try {
+                    // Effectuer la recherche de films
                     const response = await fetch('http://localhost:5000/search?query=' + this.searchQuery + '&type=movie');
                     const data = await response.json();
 
                     if (data.Response === 'True') {
-                        this.movies = data.Search;  // Si la réponse est valide, stocker les films
+                        // Pour chaque film récupéré, on va faire une seconde requête pour récupérer ses détails (Plot)
+                        const movieDetailsPromises = data.Search.map(async (movie) => {
+                            const detailsResponse = await fetch(`http://localhost:5000/search?query=${movie.imdbID}&type=movie`);
+                            const detailsData = await detailsResponse.json();
+
+                            if (detailsData.Response === 'True') {
+                                movie.plot = detailsData.Search[0].Plot; // Ajout du Plot à chaque film
+                            } else {
+                                movie.plot = 'Aucune description disponible'; // Si pas de description
+                            }
+
+                            return movie;
+                        });
+
+                        // Attendre que toutes les requêtes pour les détails soient terminées
+                        this.movies = await Promise.all(movieDetailsPromises);
                     } else {
                         this.movies = [];  // Si pas de résultats
                     }
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/search', async (req, res) => {
     }
 
     try {
+        // Récupérer la liste des films en fonction de la recherche
         const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 s: query,
@@ -27,9 +28,37 @@ app.get('/search', async (req, res) => {
             }
         });
 
-        res.json(response.data);  // Renvoie les résultats à l'application frontend
+        // Si des films sont trouvés
+        if (response.data.Response === 'True') {
+            // Pour chaque film récupéré, faire une autre requête pour obtenir les détails (Plot)
+            const movieDetailsPromises = response.data.Search.map(async (movie) => {
+                // Récupérer les détails complets du film avec imdbID
+                const detailsResponse = await axios.get('https://www.omdbapi.com/', {
+                    params: {
+                        i: movie.imdbID,  // Utiliser imdbID pour obtenir les détails complets du film
+                        apikey: OMDB_API_KEY
+                    }
+                });
+
+                // Ajouter le plot (et d'autres détails si nécessaire) au film
+                return {
+                    ...movie,
+                    plot: detailsResponse.data.Plot || 'Aucune description disponible', // Ajouter Plot ou message par défaut
+                };
+            });
+
+            // Attendre que toutes les requêtes pour les détails des films soient terminées
+            const moviesWithDetails = await Promise.all(movieDetailsPromises);
+
+            // Retourner les films enrichis (avec Plot)
+            res.json({ ...response.data, Search: moviesWithDetails });
+        } else {
+            // Si aucun film n'est trouvé, renvoyer la réponse initiale de l'OMDb API
+            res.json(response.data);
+        }
+
     } catch (error) {
-        console.error(error);
+        console.error('Erreur lors de la récupération des films:', error);
         res.status(500).send('Erreur lors de la récupération des films');
     }
 });
